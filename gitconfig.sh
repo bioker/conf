eff0fd424550c47026dc198e0f0771c0f2bc4ad2
Viktor Vlasov <viktorvlasovsiberian@gmail.com>
2020-03-30 23:44:20 +0300

    git config script

---
+++ gitconfig.sh
@@ -0,0 +1,8 @@
+git config --global user.name "Viktor Vlasov"
+git config --global user.email "viktorvlasovsiberian@gmail.com"
+git config --global core.autocrlf input
+git config --global core.quotepath false
+git config --global push.default simple
+git config --global diff.tool vimdiff
+git config --global user.signingkey C1A1019C
+git config --global commit.gpgsign true
