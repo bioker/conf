a49756253846c78d820c2fd09bd411ffe4ba6ec9
Viktor Vlasov <viktorvlasovsiberian@gmail.com>
2020-08-06 10:04:44 +0300

    vimdiff as git difftool

--- configure_git.sh
+++ configure_git.sh
@@ -3,6 +3,7 @@ git config --global user.email 'viktorvlasovsiberian@gmail.com'
 
 git config --global core.autocrlf input
 git config --global core.quotepath false
+git config --global diff.tool 'vimdiff'
 git config --global core.pager 'delta --keep-plus-minus-markers --plus-color="#012800" --minus-color="#340001"'
 git config --global interactive.diffFilter 'delta --color-only'
 
