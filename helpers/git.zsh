12a8d73571f9c1e79bb6f2bb0e33de0f09203a31
Viktor Vlasov <viktorvlasovsiberian@gmail.com>
2021-03-09 09:01:02 +0200

    git aliases + pylint rc file

--- helpers/git.zsh
+++ helpers/git.zsh
@@ -8,7 +8,7 @@ alias gdev='git checkout develop'
 alias gic='git checkout --'
 alias gis='git status'
 alias gipl='git pull'
-alias giph='git push'
+alias gipsh='git push'
 alias gidh='git diff HEAD'
 alias gicb="git branch -a | fzf | tr -d '[:space:]' | tr -d '*' | xargs git checkout"
 alias gicdf="echo \"clean all non-versioned files?\" && [[ 'yes' == \"\$(read temp; echo \$temp)\" ]] && git clean -df"
@@ -17,6 +17,10 @@ alias gidco="git log --left-right --cherry-pick --oneline"
 abbr -S -g --force --quiet gidm='git merge --no-commit --no-ff'
 abbr -S -g --force --quiet gima='git merge --abort'
 abbr -S -g --force --quiet gicmb='git branch --merged | grep -v "master\|develop\|main" | xargs git branch -d'
+abbr -S -g --force --quiet gicom='git commit -m'
+abbr -S -g --force --quiet gich='git checkout'
+abbr -S -g --force --quiet gichb='git checkout -b'
+abbr -S -g --force --quiet gipr='gh pr create --base develop --head $(git rev-parse --abbrev-ref HEAD)'
 
 function git_diff_commits {
     ref_left=$1
