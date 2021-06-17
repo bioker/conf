e0368d0f68469960e7ad09b02be8a054519dae7c
Viktor Vlasov <viktorvlasovsiberian@gmail.com>
2021-06-17 18:04:18 +0300

    docker prune alias

--- helpers/docker.zsh
+++ helpers/docker.zsh
@@ -5,3 +5,4 @@ alias dex="sudo docker exec -it"
 alias dl="sudo docker logs"
 alias ds='sudo docker stop'
 alias dsa='sudo docker stop $(sudo docker ps -qa)'
+alias dpru='sudo docker container prune; sudo docker volume prune; sudo docker image prune'
