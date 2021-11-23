8530463d031c77550f23ec69f2f272cb31ee4069
Viktor Vlasov <viktorvlasovsiberian@gmail.com>
2021-11-23 21:23:12 +0200

    remove unused programs from installation script

--- configure_system.sh
+++ configure_system.sh
@@ -21,18 +21,6 @@ echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt update && sudo apt install google-cloud-sdk
 
-wget https://go.skype.com/skypeforlinux-64.deb
-sudo apt install -y ./skypeforlinux-64.deb
-rm ./skypeforlinux-64.deb
-
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.4.3-amd64.deb
-sudo apt install -y ./slack-desktop-*.deb
-rm ./slack-desktop-*.deb
-
-wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-sudo apt install -y ./teamviewer_amd64.deb
-rm ./teamviewer_amd64.deb
-
 wget https://github.com/dandavison/delta/releases/download/0.1.1/git-delta_0.1.1_amd64.deb
 sudo apt install -y ./git-delta_0.1.1_amd64.deb
 rm ./git-delta_0.1.1_amd64.deb
@@ -47,18 +35,8 @@ sudo mv kubelogin /usr/local/bin/kubectl-oidc_login
 rm kubelogin_linux_amd64.zip
 rm LICENSE
 
-curl -LO https://github.com/psycofdj/yaml-path/releases/download/v0.4/yaml-path-0.4.linux-amd64.tar.gz
-cat yaml-path-0.4.linux-amd64.tar.gz | tar xz
-sudo mv yaml-path-0.4.linux-amd64/yaml-path /usr/local/bin
-rm -r yaml-path-0.4.linux-amd64
-rm -r yaml-path-0.4.linux-amd64.tar.gz
-
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-mkdir -p /home/wls/Programs/jmxterm
-curl -L https://github.com/jiaqi/jmxterm/releases/download/v1.0.1/jmxterm-1.0.1-uber.jar -o /home/wls/Programs/jmxterm/jmxterm-1.0.1-uber.jar
-ln -s /home/wls/Programs/jmxterm/jmxterm-1.0.1-uber.jar /home/wls/Programs/jmxterm/jmxterm.jar
 
 curl -LO https://releases.hashicorp.com/vault/1.4.2/vault_1.4.2_linux_amd64.zip
 unzip vault_1.4.2_linux_amd64.zip
@@ -76,12 +54,6 @@ unzip gradle-6.6.1-20200904230712+0000-bin.zip
 mv gradle-6.6.1-20200904230712+0000 /home/wls/Programs
 ln -s /home/wls/Programs/gradle-6.6.1-20200904230712+0000 /home/wls/Programs/gradle
 rm -r gradle-6.6.1-20200904230712+0000-bin.zip
-
-curl -LO https://dl.bintray.com/groovy/maven/apache-groovy-binary-3.0.5.zip
-unzip apache-groovy-binary-3.0.5.zip
-mv groovy-3.0.5 /home/wls/Programs
-ln -s /home/wls/Programs/groovy-3.0.5 /home/wls/Programs/groovy
-rm -r apache-groovy-binary-3.0.5.zip
 
 curl -LO https://download.java.net/java/GA/jdk12.0.2/e482c34c86bd4bf8b56c0b35558996b9/10/GPL/openjdk-12.0.2_linux-x64_bin.tar.gz
 cat openjdk-12.0.2_linux-x64_bin.tar.gz | tar xz
