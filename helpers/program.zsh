e6eec5c0ddba5fdaaf7189890b690524cbd29e3d
Viktor Vlasov <viktorvlasovsiberian@gmail.com>
2023-07-23 20:16:36 +0300

    add android development environment configuration

--- helpers/program.zsh
+++ helpers/program.zsh
@@ -1,11 +1,14 @@
-export PATH=$PATH:/home/wls/Programs/evans
-export PATH=$PATH:/home/wls/Programs/go/bin
-export PATH=$PATH:/home/wls/.go/bin
-export PATH=$PATH:/home/wls/Programs/kotlinc/bin
-export PATH=$PATH:/home/wls/Programs/kafka/bin
-
 export GOPATH=/home/wls/.go
 export NVM_DIR="$HOME/.nvm"
+export ANDROID_HOME="$HOME/Android/Sdk"
+
+export PATH=$PATH:$HOME/Programs/evans
+export PATH=$PATH:$HOME/Programs/go/bin
+export PATH=$PATH:$HOME/.go/bin
+export PATH=$PATH:$HOME/Programs/kotlinc/bin
+export PATH=$PATH:$HOME/Programs/kafka/bin
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
